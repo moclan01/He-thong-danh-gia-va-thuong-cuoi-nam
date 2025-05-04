13241fb869c067b215ac8b7a1aeffeae2438e169
Moc Lan <milepro98@gmail.com>
2025-05-05 03:56:18 +0700

    update project

--- back-end/app/Http/Controllers/AuthController.php
+++ back-end/app/Http/Controllers/AuthController.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    private $allowedRoles = ['admin', 'director', 'supervisor', 'manager', 'employee'];
 
     public function login(Request $request)
     {
